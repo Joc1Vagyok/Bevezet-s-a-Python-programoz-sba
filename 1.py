1166123e75d32c034682fa7db6e8f6d5a1094c49
Varjasi <szemanmarton4@gmail.com>
2023-11-08 11:02:25 +0100

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+x = print("Hello")
+print(x)
+
+def fv(num):
+    if (num>5) or (num==3):
+        return 0
+    else:
+        print("kisebb")
+    pass 
+    #return 0
+    
+y = fv(4)
+print(y)
+# Tabulátor
+print("Python", end=', ')
+
+# Elválasztó karakter
+
+print("Python", sep="*")
